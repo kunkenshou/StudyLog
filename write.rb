a1595910d84bfd504c534249f9a7e37d506599cc
kunkenshou <kunkenshou@gmail.com>
2023-06-10 07:35:52 +0900

    0610

--- write.rb
+++ write.rb
@@ -3,5 +3,5 @@ require "readline"
 #ファイルに書き込む処理
   puts "・メモを書いてください。"
   content = Readline.readline("メモを入力してEnterを押すと保存されます :")
-  File.open("log.txt", "w", 0755) do |file| file.write(content)
+  File.open("./log/log.txt", "w", 0755) do |file| file.write(content)
   end
