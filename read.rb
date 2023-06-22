53bc42f5737e979972f227fe78c50b3ac4962e21
kunkenshou <kunkenshou@gmail.com>
2023-06-22 20:34:02 +0900

    normalized関数をReadNormalizedClassクラスへ処理を切り出し

--- read.rb
+++ read.rb
@@ -1,42 +1,43 @@
-  puts "メモのタイトル一覧"
-  #メモの保存ディレクトリを配列化
-  file_dir = Dir.entries($dir_path)
-  
-  #不要な要素を取り除いて、配列を再生成
-  file_dir = file_dir.drop(2)
-  
-  #メモの保存ディレクトリを標準出力
-  file_dir.each do |entry| 
-  puts entry
-  end
+require "./normalized.rb"
+require "./path.rb"
+
+puts "メモのタイトル一覧"
+
+
+  dir_obj = DirClass.new
+  puts dir_obj.dir_path($file_dir)
   
   #メモのタイトルがディレクトリに存在するタイトルが入力されたら戻り値をtureを返してる繰り返しの処理を抜ける
   loop do
   #メモのタイトルを標準入力で受け取る
-  file_name = Readline.readline("メモのタイトルを入力してください。＞ :")
-  
-  #file_nameを正規表現でマッチング、変数に真偽値を格納
-  normalized_input = file_name.match?($pattern)
-  
-  #file_dirの配列から標準入力されたタイトルを比較、変数に真偽値を格納
-  value_exists = file_dir.include?(file_name)
+  $file_name = Readline.readline("メモのタイトルを入力してください。＞ :")
 
+#file_nameを正規表現でマッチング、変数に真偽値を格納
+$normalized_input = $file_name.match?($pattern)
+
+#file_dirの配列から標準入力されたタイトルを比較、変数に真偽値を格納
+$value_exists = $file_dir.include?($file_name)
+  
+  readnormalied_obj = ReadNormalizedClass.new
+  normalized_input = readnormalied_obj.read_normalized($value_exists, $normalized_input)
+  
+=begin
 #関数処理、引数の型によって別て戻り値に真偽値を返す
 def normalized(value_exists, normalized_input)
-  if value_exists == true
+  if $value_exists == true
     return true
-  elsif normalized_input == false
+  elsif $normalized_input == false
     return false
   end
 end
 
-
 #関数の戻り値が変数に代入
-normalized_input = normalized(value_exists, normalized_input)
+normalized_input = normalized($value_exists, $normalized_input)
+=end
 
   #真偽値で処理を分岐、tureなら標準入力で受け取ったタイトルのメモの内容を表示する、falseならメモを見るの処理に戻る
   if normalized_input == true
-  file_path = File.join($dir_path, file_name)
+  file_path = File.join($dir_path, $file_name)
   file = File.read(file_path)
   puts file
   else
