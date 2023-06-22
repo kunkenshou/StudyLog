31425a7c66be50949fdfb1f356a15f96e3484d0b
kunkenshou <kunkenshou@gmail.com>
2023-06-22 23:50:29 +0900

    共通で使用する配列や変数を切り出して格納

--- path.rb
+++ path.rb
@@ -1,3 +1,4 @@
+
 #共通部分
 
 #標準入力で受け取った値に例外が発生させない様に正規表現を変数に代入
@@ -8,6 +9,13 @@ $menu_array = Array(["1", "2", "3", "4"])
 
 #メモの保存先のディレクトリを指定
 $dir_path = './log'
+
+#メモの保存ディレクトリを配列化
+$file_dir = Dir.entries($dir_path)
+
+#不要な要素を取り除いて、配列を再生成
+$file_dir = $file_dir.drop(2)
+
 
 #$input_length = $input.length
 
