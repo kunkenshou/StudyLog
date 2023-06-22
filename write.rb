67f41ae8af023267946a3e0c842f04b3338eac7f
kunkenshou <kunkenshou@gmail.com>
2023-06-22 23:46:25 +0900

    ディレクトリを出力/入力値の正規化処理をnormalizedを切り出し

--- write.rb
+++ write.rb
@@ -1,37 +1,29 @@
+require "./normalized.rb"
+require "./path.rb"
+
 #入力で受け取った値を評価して結果がtureになるまで処理を繰り返す
 loop do
 
-    #メモの保存ディレクトリを配列化
-    file_dir = Dir.entries($dir_path)
-  
-    #dropメソッドで不要な要素を取り除いて配列を生成
-    file_dir = file_dir.drop(2)
+#メモの保存ディレクトリ配列、Array型が格納
+dir_obj = DirClass.new 
+file_dir = dir_obj.filedir($dir_path)
 
 #入力で値を受け取る
   file_name = Readline.readline("メモのタイトルを入力してください。＞ :")
 
   #file_nameを正規表現でマッチング、変数に真偽値を格納
-  normalized_input = file_name.match?($pattern)
+  $normalized_input = file_name.match?($pattern)
 
   file_name = file_name + ".txt"
 
   #file_dirの配列から標準入力されたタイトルを比較、変数に真偽値を格納
- value_exists = file_dir.include?(file_name)
+ $value_exists = file_dir.include?(file_name)
 
-#関数処理、引数の型によって別て戻り値に真偽値を返す
-def normalized(value_exists, normalized_input)
-  case
-  when value_exists == true || normalized_input == true
-    return true
-  when value_exists == false || normalized_input == true
-    return false
-  when value_exists == false || normalized_input == false
-    return false
-  end
-end
+#入力値の正規化する処理
+normalized_obj = NormalizedClass.new
+#関数の戻り値を変数に代入
+normalized_input = normalized_obj.write_normalized($value_exists, $normalized_input)
 
-  #関数の戻り値が変数に代入
-  normalized_input = normalized(value_exists, normalized_input)
 
   #真偽値で処理を分岐、tureなら標準入力で受け取ったタイトルのメモの内容を表示する、falseならメモを見るの処理に戻る
   if normalized_input == false
