cd76fce7ae428604d7777f84003ea2246a768888
kunkenshou <kunkenshou@gmail.com>
2023-06-24 07:35:21 +0900

    ディレクトリを出力/入力値の正規化処理をnormalizedを切り出し

--- delete.rb
+++ delete.rb
@@ -1,11 +1,15 @@
+#メモを削除する処理のファイル
+
 require "./normalized.rb"
 require "./path.rb"
 
-puts "メモのタイトル一覧"
+$filename = "削除"
+puts "#{$filename}するメモの一覧"
 
 #メモの保存ディレクトリ配列、Array型が格納
 dir_obj = DirClass.new
-#file_dir = dir_obj.filedir($dir_path)
+#NormalizedClassクラスからインスタンスを作成、normalizedオブジェクトを作成
+normalized_obj = NormalizedClass.new
 
 #メモの保存ディレクトリを表示
   puts dir_obj.dir_path($file_dir)
@@ -13,25 +17,18 @@ dir_obj = DirClass.new
   #メモのタイトルがディレクトリに存在するタイトルが入力されたら戻り値をtureを返してる繰り返しの処理を抜ける
   loop do
   #メモのタイトルを標準入力で受け取る
-  file_name = Readline.readline("削除したいファイル名を入力してください。＞ :")
+  $file_name = Readline.readline("削除したいファイル名を入力してください。＞ :")
 
-#file_nameを正規表現でマッチング、変数に真偽値を格納
-$normalized_input = file_name.match?($pattern)
 
-#file_dirの配列から標準入力されたタイトルを比較、変数に真偽値を格納
-$value_exists = $file_dir.include?(file_name)
-
-#NormalizedClassクラスからインスタンスを作成、normalizedオブジェクトを作成
-normalized_obj = NormalizedClass.new
 #関数の戻り値が変数に代入
-normalized_input = normalized_obj.delete_normalized($value_exists, $normalized_input)
+normalized_input = normalized_obj.title_normalized($value_exists, $normalized_input)
 
 
 #真偽値で処理を分岐、tureなら標準入力で受け取ったタイトルのメモの内容を表示する、falseならメモを見るの処理に戻る
 if normalized_input == true
-  file_path = File.join($dir_path, file_name)
+  file_path = File.join($dir_path, $file_name)
   File.delete(file_path)
-  puts "#{file_name}を削除しました。" if File.delete == 0
+  puts "#{$file_name}を削除しました。" if File.delete == 0
   else
     puts "ファイル名が存在しません。もう一度入力してください。"
   end
