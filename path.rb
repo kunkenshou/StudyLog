b0d02f75baa2e2fb4949e54feb4b290f0f991f69
kunkenshou <kunkenshou@gmail.com>
2023-06-24 07:38:14 +0900

    共通で使用する配列や変数を切り出して格納

--- path.rb
+++ path.rb
@@ -1,11 +1,16 @@
 
 #共通部分
 
-#標準入力で受け取った値に例外が発生させない様に正規表現を変数に代入
+#入力値の正規化用のパターン、読み込み処理、削除用
 $pattern = /[%.+\/\p{Space}\s]/
 
+#入力値の正規化用のパターン、書き込み用
+$pattern1 = /[%+\/\p{Space}\s]/
+
 #メニューの番号
-$menu_array = Array(["1", "2", "3", "4"])
+$menu_array = Array(["1", "2", "3", "4", "5"])
+
+
 
 #メモの保存先のディレクトリを指定
 $dir_path = './log'
@@ -16,7 +21,3 @@ $file_dir = Dir.entries($dir_path)
 #不要な要素を取り除いて、配列を再生成
 $file_dir = $file_dir.drop(2)
 
-
-#$input_length = $input.length
-
-$hoge = "リトルインディアン"
