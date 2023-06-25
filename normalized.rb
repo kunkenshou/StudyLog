b853685b3c6c94b6174909619bc97063eeed8e2b
ogasawara <lowta84@gmail.com>
2023-06-25 11:53:02 +0900

    menu_normalizedの引数について

--- normalized.rb
+++ normalized.rb
@@ -45,15 +45,15 @@ class NormalizedClass
     else
       return true
     end
-    
-    
+
+
     when $filename == "メモの作成"
           #file_nameを正規表現でマッチング、変数に真偽値を格納
     #正規化、記号とマッチするとture
     $normalized_input = $file_name.match?($pattern)
 
     $file_name = $file_name + ".txt"
-    
+
     #file_dirの配列から標準入力されたタイトルを比較、変数に真偽値を格納
     $value_exists = $file_dir.include?($file_name)
 
@@ -63,22 +63,23 @@ class NormalizedClass
       return false
     else
       return true
+    end
   end
+
+  #標準入力で入力内容に記号が含まれていないか、文字列が1文字以上ないか確認する関数。戻り値は期待値以外はtrue、期待値はstring
+  #input_lengthの引数は文字数じゃないとだめですか？例えばinputがnullかどうかで判定するなどでは不都合があるのでしょうか？
+  def menu_normalized(input, pattern, input_length, menu_array)
+    case
+    when $input.match?($pattern) == true #menuに記号がないか比較して評価する
+      return true
+    when ($input_length == 1) == false #menuの入力値長さが1文字以上か比較して評価する
+      return true
+    when $menu_array.include?($input) == false #menuにない数値が入力された比較して評価する
+      return true
+    else
+      return menu_number = $input #menuの数が入力されたら、変数に代入する
+    end
   end
-  
-#標準入力で入力内容に記号が含まれていないか、文字列が1文字以上ないか確認する関数。戻り値は期待値以外はtrue、期待値はstring
-def menu_normalized(input, pattern, input_length, menu_array)
-  case
-  when $input.match?($pattern) == true #menuに記号がないか比較して評価する
-    return true
-  when ($input_length == 1) == false #menuの入力値長さが1文字以上か比較して評価する
-    return true
-  when $menu_array.include?($input) == false #menuにない数値が入力された比較して評価する
-    return true
-  else
-    return menu_number = $input #menuの数が入力されたら、変数に代入する
-  end
-end
 end
 
 class DirClass
@@ -86,16 +87,17 @@ class DirClass
   def filedir(dir_path)
       #メモの保存ディレクトリを配列化
     file_dir = Dir.entries($dir_path)
-  
+
+
     #dropメソッドで不要な要素を取り除いて配列を生成
     file_dir = file_dir.drop(2)
-    
+
     return file_dir
   end
-  
+
   #メモの保存ディレクトリ配列、Array型が格納
   def dir_path(file_dir)
-  $file_dir.each do |entry| 
+  $file_dir.each do |entry|
   end
   end
 end
